3c29afbb181fe0e632465d8319d5f73dd9fc0c3f
shahbaz12ilyas <49778622+shahbaz12ilyas@users.noreply.github.com>
2019-05-15 00:36:40 +0500

    shahbazilyas

---
+++ shahbazilyas.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500,500))
+ 
+run=True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run=False 
+
+    pygame.display.update()
+
+
+pygame.quit()
